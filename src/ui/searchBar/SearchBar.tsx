ae3f7e740c3b04b75d236b3f10026f154e827fa4
Fabián Sánchez <faalsaru@gmail.com>
2025-04-03 21:11:11 -0500

    feat(searchBar): implementing search bar by name functionality

--- src/ui/searchBar/SearchBar.tsx
+++ src/ui/searchBar/SearchBar.tsx
@@ -1,21 +1,35 @@
-import { FC } from 'react'
-import searchIcon from '../../assets/search.svg'
-import { FilterButton } from '../../features/filters'
+import { FC, useEffect, useState } from "react";
+import searchIcon from "../../assets/search.svg";
+import { FilterButton } from "../../features/filters";
+import { useSetFilter } from "../../features/filters/store/filter.store";
 
 export type SearchBarProps = {
-  placeholder?: string
-  onChange?: (value: string) => void
-  value?: string
-}
+  placeholder?: string;
+};
 
-export const SearchBar: FC<SearchBarProps> = ({placeholder}) => {
+export const SearchBar: FC<SearchBarProps> = ({ placeholder }) => {
+  const setFilter = useSetFilter();
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      setFilter("name", searchValue.trim());
+    }, 300); 
+
+    return () => clearTimeout(delayDebounce);
+  }, [searchValue]);
 
   return (
     <div className="px-6 py-3 mb-8 gap-3 flex bg-gray-100 rounded-lg relative">
       <img src={searchIcon} alt="Search icon" />
-      <input className='font-medium text-sm w-full outline-none' type="text" placeholder={placeholder} />
+      <input
+        className="font-medium text-sm w-full outline-none"
+        type="text"
+        placeholder={placeholder}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
       <FilterButton />
-
     </div>
-  )
-}
+  );
+};
